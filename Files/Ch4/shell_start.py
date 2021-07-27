9dabe056bca6e79d9f90ddf0a0f5ac27c7a3ad2a
diego <dmpastuch@gmail.com>
2021-07-27 11:26:47 -0300

    update

--- Files/Ch4/shell_start.py
+++ Files/Ch4/shell_start.py
@@ -3,27 +3,28 @@
 #
 import os
 from os import path
+import shutil
+
 
 def main():
-  # make a duplicate of an existing file
-  if path.exists("textfile.txt"):
-    # get the path to the file in the current directory
+    # make a duplicate of an existing file
+    if path.exists("textfile.txt"):
+        # get the path to the file in the current directory
+        src = path.realpath("textfile.txt")
 
-    
-    # let's make a backup copy by appending "bak" to the name
+        # let's make a backup copy by appending "bak" to the name
+        dst = src + ".bak"
 
-    
-    # copy over the permissions, modification times, and other info
+        # copy over the permissions, modification times, and other info
+        shutil.copy(src, dst)
+        shutil.copystat(src, dst)
 
-    
-    # rename the original file
+        # rename the original file
 
-    
-    # now put things into a ZIP archive
+        # now put things into a ZIP archive
+
+        # more fine-grained control over ZIP files
 
 
-    # more fine-grained control over ZIP files
-
-      
 if __name__ == "__main__":
-  main()
+    main()
