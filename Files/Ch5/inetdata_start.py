ef09cb0b5bf7a9d79bd66c2a35a35336fb08dcf3
diego <dmpastuch@gmail.com>
2021-07-27 12:52:35 -0300

    file updated

--- Files/Ch5/inetdata_start.py
+++ Files/Ch5/inetdata_start.py
@@ -1,6 +1,15 @@
-# 
+#
 # Example file for retrieving data from the internet
 #
+import urllib.request
+
+
+def main():
+    webUrl = urllib.request.urlopen("https://patagoniahappytravel.com")
+    print("result code: " + str(webUrl.getcode()))
+    data = webUrl.read()
+    print(data)
+
 
 if __name__ == "__main__":
-  main()
+    main()
